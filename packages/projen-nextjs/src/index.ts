bfb55aa78e686134355195dd7d1fd605ce1f1266
Derek Kershner <dkershner@dkershner.com>
2024-01-23 13:34:56 -0800

    fix: extends, not plugin

--- packages/projen-nextjs/src/index.ts
+++ packages/projen-nextjs/src/index.ts
@@ -51,7 +51,7 @@ export class NextjsEslint extends Component {
                 ? `@${nextDep?.version}`
                 : "";
             project.addDevDeps(`@next/eslint-plugin-next${nextVersionSuffix}`);
-            project.eslint.addPlugins("plugin:@next/next/recommended");
+            project.eslint.addExtends("plugin:@next/next/recommended");
 
             const conflictingRules = ["jsx-a11y/anchor-is-valid"];
 
